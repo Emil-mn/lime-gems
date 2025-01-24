9efd73a659d42108ef071f4c748dffcea3f6cbe3
emilMN <emil.nybergmyhre@elev.ga.ntig.se>
2025-01-24 14:19:46 +0100

    added pausing/quitting, worked on movement, and added custom cursor

--- fourth gem/fourth.js
+++ fourth gem/fourth.js
@@ -16,7 +16,8 @@ var ctx = can.getContext('2d')
 //states
 var states = {menu:0,main:1,paused:2}
 var gameState = states.menu
-var prevState
+var interval
+var canPause = true
 //skillpoint boosts on level-up???
 var skillPoints = 0
 var dmgLvl = 0,frtLvl = 0 //???
@@ -32,6 +33,7 @@ var xpReqs = [25,50,75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,4
 var spAmounts = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50]
 //character functionality
 var playerSprite
+var spriteSelection = 1
 var playerX = totW/2
 var playerY = totH/2
 var inputVector = [0,0]
@@ -50,13 +52,22 @@ var maxHealth = 100
 var maxShield = 0
 var xpRequired = xpReqs[0]
 //sprites
+var fighter
 var fighterBody
 var fighterWings
 var fighterEngines
 var fighterWindow
-var fighter
 
-function updateFigtherSprite() {
+var corvette
+var corvetteBody
+var corvetteSides
+var corvetteWings
+var corvetteEngines
+var corvetteBridge
+var corvetteWindow
+
+
+function updateFighterSprite() {
     fighterBody = new Path2D
     fighterBody.moveTo(playerX-11,playerY+40);
     fighterBody.lineTo(playerX-11,playerY-20);
@@ -100,7 +111,79 @@ function updateFigtherSprite() {
     fighterWindow.closePath();
 
     fighter = [fighterBody,fighterWings,fighterEngines,fighterWindow]
-    playerSprite = fighter
+}
+
+function updateCorvetteSprite() {
+    corvetteBody = new Path2D
+    corvetteBody.moveTo(playerX-17,playerY+50)
+    corvetteBody.lineTo(playerX-17,playerY-60)
+    corvetteBody.lineTo(playerX-7,playerY-75)
+    corvetteBody.lineTo(playerX+7,playerY-75)
+    corvetteBody.lineTo(playerX+17,playerY-60)
+    corvetteBody.lineTo(playerX+17,playerY+50)
+    corvetteBody.closePath()
+        
+    corvetteSides = new Path2D
+    corvetteSides.moveTo(playerX-17,playerY+50)
+    corvetteSides.lineTo(playerX-33,playerY+33)
+    corvetteSides.lineTo(playerX-33,playerY-13)
+    corvetteSides.lineTo(playerX-17,playerY-30)
+    corvetteSides.closePath()
+    
+    corvetteSides.moveTo(playerX+17,playerY+50)
+    corvetteSides.lineTo(playerX+33,playerY+33)
+    corvetteSides.lineTo(playerX+33,playerY-13)
+    corvetteSides.lineTo(playerX+17,playerY-30)
+    corvetteSides.closePath()
+        
+    corvetteWings = new Path2D  
+    corvetteWings.moveTo(playerX-33,playerY+15)
+    corvetteWings.lineTo(playerX-60,playerY+18)
+    corvetteWings.lineTo(playerX-60,playerY+9)
+    corvetteWings.lineTo(playerX-33,playerY)
+    corvetteWings.closePath()
+    
+    corvetteWings.moveTo(playerX+33,playerY+15)
+    corvetteWings.lineTo(playerX+60,playerY+18)
+    corvetteWings.lineTo(playerX+60,playerY+9)
+    corvetteWings.lineTo(playerX+33,playerY)
+    corvetteWings.closePath()
+        
+    corvetteEngines = new Path2D  
+    corvetteEngines.moveTo(playerX-23,playerY+44)
+    corvetteEngines.lineTo(playerX-23,playerY+50)
+    corvetteEngines.quadraticCurveTo(playerX-20,playerY+55,playerX-20,playerY+60)
+    corvetteEngines.lineTo(playerX-33,playerY+60)
+    corvetteEngines.quadraticCurveTo(playerX-33,playerY+55,playerX-30,playerY+50)
+    corvetteEngines.lineTo(playerX-30,playerY+36)
+    corvetteEngines.closePath()
+    
+    corvetteEngines.moveTo(playerX+23,playerY+44)
+    corvetteEngines.lineTo(playerX+23,playerY+50)
+    corvetteEngines.quadraticCurveTo(playerX+20,playerY+55,playerX+20,playerY+60)
+    corvetteEngines.lineTo(playerX+33,playerY+60)
+    corvetteEngines.quadraticCurveTo(playerX+33,playerY+55,playerX+30,playerY+50)
+    corvetteEngines.lineTo(playerX+30,playerY+36)
+        
+    corvetteBridge = new Path2D
+    corvetteBridge.moveTo(playerX-4,playerY+30)
+    corvetteBridge.lineTo(playerX-10,playerY+15)
+    corvetteBridge.lineTo(playerX-10,playerY-10)
+    corvetteBridge.lineTo(playerX-4,playerY-20)
+    corvetteBridge.lineTo(playerX+4,playerY-20)
+    corvetteBridge.lineTo(playerX+10,playerY-10)
+    corvetteBridge.lineTo(playerX+10,playerY+15)
+    corvetteBridge.lineTo(playerX+4,playerY+30)
+    corvetteBridge.closePath()
+        
+    corvetteWindow = new Path2D
+    corvetteWindow.moveTo(playerX-9,playerY-11)
+    corvetteWindow.lineTo(playerX-4,playerY-19)
+    corvetteWindow.lineTo(playerX+4,playerY-19)
+    corvetteWindow.lineTo(playerX+9,playerY-11)
+    corvetteWindow.closePath();
+    
+    corvette = [corvetteBody,corvetteSides,corvetteWings,corvetteEngines,corvetteBridge,corvetteWindow]
 }
 
 can.addEventListener('mousedown', function(event) {
@@ -122,6 +205,12 @@ can.addEventListener('mousemove', function(event) {
 document.addEventListener('keydown', function(event) {
     keysPressed = (keysPressed || []);
     keysPressed[event.key] = true; //console.log(keysPressed)
+    if (event.key == 'Escape' && gameState == states.paused) {
+        console.log('resumed'); gameState = states.main
+        ctx.textAlign = 'left'; can.style.cursor = 'none'
+        setTimeout(() => {canPause = true},500)
+        interval = setInterval(mainLoop,20)
+    }
 })
 document.addEventListener('keyup', function(event) {
     keysPressed[event.key] = false; //console.log(keysPressed)
@@ -199,6 +288,19 @@ function checkClick() {
             setTimeout(mainMenu,2000)
         }
     }
+    else if (gameState == states.paused) {
+        if (buttonClicked(totW/2-130,180,260,50)) {
+            console.log('resumed'); gameState = states.main
+            ctx.textAlign = 'left'; can.style.cursor = 'none'
+            setTimeout(() => {canPause = true},500)
+            interval = setInterval(mainLoop,20)
+        }
+        else if (buttonClicked(totW/2-130,270,260,50)) {
+            console.log('quitting'); gameState = states.menu;
+            canPause = true; mainMenu()
+        }
+        
+    }
 }
 
 function hoverCheck() {
@@ -213,13 +315,22 @@ function hoverCheck() {
         }
         else {can.style.cursor = 'default'}
     }
+    else if (gameState == states.paused) {
+        if (buttonHovered(totW/2-130,180,260,50)) {
+            can.style.cursor = 'pointer'
+        }
+        else if (buttonHovered(totW/2-130,270,260,50)) {
+            can.style.cursor = 'pointer'
+        }
+        else {can.style.cursor = 'default'}
+    }
 }
 
 
 function generateWorld() {
     console.log('generation stuff');
-    gameState = states.main; can.style.cursor = 'default'
-    ctx.textAlign = 'left'; setInterval(mainLoop,20)
+    gameState = states.main; can.style.cursor = 'none'
+    ctx.textAlign = 'left'; interval = setInterval(mainLoop,20)
 }
 
 
@@ -230,6 +341,9 @@ function mainLoop() {
     if (keysPressed && keysPressed['h'] && health < maxHealth) {health += 0.5}
     if (keysPressed && keysPressed['c']) {credits += 2}
     if (keysPressed && keysPressed['x']) {xp += 0.2}
+
+    if (keysPressed && keysPressed['f']) {spriteSelection = 1}
+    else if (keysPressed && keysPressed['c']) {spriteSelection = 2}
     
     //levelling up
     if (xp >= xpRequired) { 
@@ -253,47 +367,45 @@ function mainLoop() {
 
     //strafing
     if (keysPressed && (keysPressed['q'])) {
-        playerSpeed = 2
+        playerSpeed = 0.025
         inputVector = [playerSpeed * Math.cos((playerAngle+180) * Math.PI / 180),playerSpeed * Math.sin((playerAngle+180) * Math.PI / 180)]
-        movementVector = inputVector
+        console.log(inputVector)
+        movementVector[0] += inputVector[0]
+        movementVector[1] += inputVector[1]
     }
     else if (keysPressed && (keysPressed['e'])) {
-        playerSpeed = 2
+        playerSpeed = 0.025
         inputVector = [playerSpeed * Math.cos((playerAngle) * Math.PI / 180),playerSpeed * Math.sin((playerAngle) * Math.PI / 180)]
-        movementVector = inputVector
+        movementVector[0] += inputVector[0]
+        movementVector[1] += inputVector[1]
     }
 
     //go forwards / backwards
     if (keysPressed && (keysPressed['w'] || keysPressed['ArrowUp'])) {
-        if (playerSpeed < playerMaxSpeed) {
+        /*if (playerSpeed < playerMaxSpeed) {
             playerSpeed += 0.1
-        }
+        }*/
+        playerSpeed = 0.05
         inputVector = [playerSpeed * Math.cos((playerAngle-90) * Math.PI / 180),playerSpeed * Math.sin((playerAngle-90) * Math.PI / 180)]
-        movementVector = inputVector
+        console.log(inputVector)
+        movementVector[0] += inputVector[0]
+        movementVector[1] += inputVector[1]
     }
     else if (keysPressed && (keysPressed['s'] || keysPressed['ArrowDown'])) {
-        if (playerSpeed > -playerMaxSpeed/2) {
-            playerSpeed -= 0.1
-        }
-        inputVector = [playerSpeed * Math.cos((playerAngle-90) * Math.PI / 180),playerSpeed * Math.sin((playerAngle-90) * Math.PI / 180)]
-        movementVector = inputVector
+        playerSpeed = 0.025
+        inputVector = [playerSpeed * Math.cos((playerAngle+90) * Math.PI / 180),playerSpeed * Math.sin((playerAngle+90) * Math.PI / 180)]
+        
+        movementVector[0] += inputVector[0]
+        movementVector[1] += inputVector[1]
     }
-    /*else {
-        if (playerSpeed > 0.01) {
-            playerSpeed -= 0.05
-        }
-        else if (playerSpeed < -0.01) {
-            playerSpeed += 0.05
-        }
-        else {
-            playerSpeed = 0
-        }
-        playerXmovement = playerSpeed * Math.cos((playerAngle - 90) * Math.PI / 180)
-        playerYmovement = playerSpeed * Math.sin((playerAngle - 90) * Math.PI / 180)
-    }*/
-
     
-
+    if (keysPressed && keysPressed['r']) {
+        if (movementVector[0] > 0) {movementVector[0] -= 0.1}
+        else if (movementVector[0] < 0) {movementVector[0] += 0.1}
+        
+        if (movementVector[1] > 0) {movementVector[1] -= 0.1}
+        else if (movementVector[1] < 0) {movementVector[1] += 0.1}
+    }
 
     //move player
     playerX += movementVector[0];
@@ -307,94 +419,26 @@ function mainLoop() {
     ctx.strokeStyle = shipStroke; 
     ctx.fillStyle = shipFill
     ctx.lineWidth = 2
-    if (keysPressed && (keysPressed['r'])) {
-        updateFigtherSprite()
-        playerSprite.forEach((path,index) => {
-            if (index == playerSprite.length-1) {
-                ctx.fillStyle = shipWindows;
-                ctx.fill(path)
-            }
-            else {
-                ctx.fill(path); ctx.stroke(path)
-            }
-        })
-    }
-    else {
-        ctx.beginPath()
-        ctx.moveTo(playerX-17,playerY+50)
-        ctx.lineTo(playerX-17,playerY-60)
-        ctx.lineTo(playerX-7,playerY-75)
-        ctx.lineTo(playerX+7,playerY-75)
-        ctx.lineTo(playerX+17,playerY-60)
-        ctx.lineTo(playerX+17,playerY+50)
-        ctx.closePath()
-        ctx.fill(); ctx.stroke()
+    
+    if (spriteSelection == 1) {updateFighterSprite(); playerSprite = fighter}
+    else if (spriteSelection == 2) {updateCorvetteSprite(); playerSprite = corvette}
 
-        ctx.beginPath()
-        ctx.moveTo(playerX-17,playerY+50)
-        ctx.lineTo(playerX-33,playerY+33)
-        ctx.lineTo(playerX-33,playerY-13)
-        ctx.lineTo(playerX-17,playerY-30)
-        ctx.closePath()
-        ctx.moveTo(playerX+17,playerY+50)
-        ctx.lineTo(playerX+33,playerY+33)
-        ctx.lineTo(playerX+33,playerY-13)
-        ctx.lineTo(playerX+17,playerY-30)
-        ctx.closePath()
-        ctx.fill(); ctx.stroke()
-
-        ctx.beginPath()
-        ctx.moveTo(playerX-33,playerY+15)
-        ctx.lineTo(playerX-60,playerY+18)
-        ctx.lineTo(playerX-60,playerY+9)
-        ctx.lineTo(playerX-33,playerY)
-        ctx.closePath()
-        ctx.moveTo(playerX+33,playerY+15)
-        ctx.lineTo(playerX+60,playerY+18)
-        ctx.lineTo(playerX+60,playerY+9)
-        ctx.lineTo(playerX+33,playerY)
-        ctx.closePath()
-        ctx.fill(); ctx.stroke()
-
-        ctx.beginPath()
-        ctx.moveTo(playerX-23,playerY+44)
-        ctx.lineTo(playerX-23,playerY+50)
-        ctx.quadraticCurveTo(playerX-20,playerY+55,playerX-20,playerY+60)
-        ctx.lineTo(playerX-33,playerY+60)
-        ctx.quadraticCurveTo(playerX-33,playerY+55,playerX-30,playerY+50)
-        ctx.lineTo(playerX-30,playerY+36)
-        ctx.closePath()
-        ctx.moveTo(playerX+23,playerY+44)
-        ctx.lineTo(playerX+23,playerY+50)
-        ctx.quadraticCurveTo(playerX+20,playerY+55,playerX+20,playerY+60)
-        ctx.lineTo(playerX+33,playerY+60)
-        ctx.quadraticCurveTo(playerX+33,playerY+55,playerX+30,playerY+50)
-        ctx.lineTo(playerX+30,playerY+36)
-        ctx.fill(); ctx.stroke()
-
-        ctx.beginPath()
-        ctx.moveTo(playerX-4,playerY+30)
-        ctx.lineTo(playerX-10,playerY+15)
-        ctx.lineTo(playerX-10,playerY-10)
-        ctx.lineTo(playerX-4,playerY-20)
-        ctx.lineTo(playerX+4,playerY-20)
-        ctx.lineTo(playerX+10,playerY-10)
-        ctx.lineTo(playerX+10,playerY+15)
-        ctx.lineTo(playerX+4,playerY+30)
-        ctx.closePath()
-        ctx.fill(); ctx.stroke()
-
-        ctx.fillStyle = shipWindows
-        ctx.beginPath()
-        ctx.moveTo(playerX-9,playerY-11)
-        ctx.lineTo(playerX-4,playerY-19)
-        ctx.lineTo(playerX+4,playerY-19)
-        ctx.lineTo(playerX+9,playerY-11)
-        ctx.closePath(); ctx.fill()
-    }
+    playerSprite.forEach((path,index) => {
+        if (index == playerSprite.length-1) {
+            ctx.fillStyle = shipWindows;
+            ctx.fill(path)
+        }
+        else {
+            ctx.fill(path); ctx.stroke(path)
+        }
+    })
     ctx.restore();
 
     ctx.fillStyle = 'red'; ctx.fillRect(playerX-1,playerY-1,2,2)
+    ctx.save(); ctx.translate(500,30); ctx.rotate(playerAngle * Math.PI / 180);
+    ctx.beginPath(); ctx.moveTo(-10,10); ctx.lineTo(10,10);
+    ctx.lineTo(0,-15); ctx.fill(); ctx.translate(-500,-30);
+    ctx.restore()
     //hud
 
     //xp bar
@@ -450,4 +494,32 @@ function mainLoop() {
     ctx.arc(totW-160+playerX/4.66,10+playerY/4.66,2,0,7); ctx.fill()
     ctx.fillStyle = foreground; ctx.font = '10px consolas'
     ctx.fillText('X:'+Math.round(playerX*10)/10+' Y:'+Math.round(playerY*10)/10,545,105)
+
+    //cursor
+    ctx.strokeStyle = foreground; ctx.lineWidth = 2; ctx.beginPath();
+    ctx.moveTo(mousePosX-2,mousePosY-6); ctx.lineTo(mousePosX-6,mousePosY-6); 
+    ctx.lineTo(mousePosX-6,mousePosY-2)
+    ctx.moveTo(mousePosX-6,mousePosY-2); ctx.lineTo(mousePosX-6,mousePosY+6); 
+    ctx.lineTo(mousePosX-2,mousePosY+6)
+    ctx.moveTo(mousePosX+2,mousePosY+6); ctx.lineTo(mousePosX+6,mousePosY+6); 
+    ctx.lineTo(mousePosX+6,mousePosY+2)
+    ctx.moveTo(mousePosX+6,mousePosY-2); ctx.lineTo(mousePosX+6,mousePosY-6); 
+    ctx.lineTo(mousePosX+2,mousePosY-6)
+    ctx.arc(mousePosX,mousePosY,2,0,7); ctx.stroke()
+
+    if (keysPressed && keysPressed['Escape'] && canPause == true) {
+        clearInterval(interval); console.log('paused');
+        gameState = states.paused; canPause = false
+        
+        ctx.fillStyle = background; ctx.fillRect(totW/2-175,totH/2-125,350,250);
+        ctx.fillStyle = foreground ; ctx.font = '50px consolas'; 
+        ctx.textAlign = 'center'; ctx.fillText('Paused',totW/2,150);
+        ctx.strokeStyle = foreground; ctx.lineWidth = 4
+        ctx.strokeRect(totW/2-175,totH/2-125,350,250)
+        ctx.strokeRect(totW/2-130,180,260,50)
+        ctx.strokeRect(totW/2-130,270,260,50)
+        ctx.font = '40px consolas'
+        ctx.fillText('Resume', totW/2,215)
+        ctx.fillText('Save & quit',totW/2,305)
+    }
 }
