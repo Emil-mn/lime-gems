17595a2eac196e87b5c3e086ea97d11309ed3391
emilMN <emil.nybergmyhre@elev.ga.ntig.se>
2025-03-14 15:00:03 +0100

    added validation

--- fourth gem/register.php
+++ fourth gem/register.php
@@ -1,18 +1,17 @@
 <?php
     //open and check db connection
-    $connection = new mysqli("localhost", "root", "loremipsum", "accounts");
+    $connection = new mysqli("localhost", "root", "loremipsum", "lime_gems");
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
     }
     echo "Connected successfully <br>";
     
     $username = $_POST["username"];
-    $password = $_POST["table"];
+    $password = $_POST["password"];
     $repeat = $_POST["repeat"];
 
     //validation
-    //1 check if username is unique   get username from database if it exists. if true show error(how?), otherwise continue
-    //check if password is same in both places???  maybe do that with js??? can probably just validate password with pattern 
+    //check if username is unique   get username from database if it exists. if true show error(close the tab and return something???), otherwise continue
     $regQuery = "INSERT INTO logins (Username, 'Password') VALUES ('$username', '$password')";
     if ($connection->query($regQuery) === TRUE) {
         echo "New account created successfully <br>";
